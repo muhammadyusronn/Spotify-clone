1119941eb5b3fee70b15a75764896ca4baf86320
user <user@MacBook-Pro-user.local>
2026-02-27 09:34:28 +0700

    UPDATED : ADD Play music

---
+++ frontend/src/layout/components/AudioPlayer.tsx
@@ -0,0 +1,46 @@
+import { usePlayerStore } from "@/stores/usePlayerStore";
+import { useEffect, useRef } from "react";
+
+const AudioPlayer = () => {
+  const audioRef = useRef<HTMLAudioElement>(null);
+  const prevSongRef = useRef<string | null>(null);
+  const { currentSong, isPlaying, playNext } = usePlayerStore();
+
+  // Handle play and pause logic
+  useEffect(() => {
+    if (isPlaying) audioRef.current?.play();
+    else audioRef.current?.pause();
+  }, [isPlaying]);
+
+  // Handle song ends
+  useEffect(() => {
+    const audio = audioRef.current;
+
+    const handleEnded = () => {
+      playNext();
+    };
+
+    audio?.addEventListener("ended", handleEnded);
+    return () => audio?.removeEventListener("ended", handleEnded);
+  }, [playNext]);
+
+  // Handle song changes
+  useEffect(() => {
+    if (!audioRef.current || !currentSong) return;
+    const audio = audioRef.current;
+
+    // check if this is actually a new song
+    const isSongChanges = prevSongRef.current !== currentSong?.audioUrl;
+    if (isSongChanges) {
+      audio.src = currentSong?.audioUrl;
+      // Reset the playback position
+      audio.currentTime = 0;
+      prevSongRef.current = currentSong?.audioUrl;
+      if (isPlaying) audio.play();
+    }
+  }, [currentSong, isPlaying]);
+
+  return <audio ref={audioRef} />;
+};
+
+export default AudioPlayer;
